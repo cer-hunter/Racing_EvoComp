9bc50264f56b217ae438b33659d07781ee54d1a3
Hunter Ceranic <ceranic.ind@gmail.com>
2023-11-15 15:07:28 -0500

    Added file for controller to be evolved

---
+++ car_racing_controller.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+import pygame
+import deap #place holder for now i dont know if we will want to use deap or pymoo or whatever
+
+import gymnasium
+import car_racing_edited
+
