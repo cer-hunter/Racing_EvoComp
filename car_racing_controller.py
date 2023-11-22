a4fee90264f9556ff7faacb3a552cd4d9ebf34a7
Hunter Ceranic <ceranic.ind@gmail.com>
2023-11-22 11:52:22 -0500

    update carracing

--- car_racing_controller.py
+++ car_racing_controller.py
@@ -40,8 +40,8 @@ def truncate(number, decimals=0):
 obs_size = 3 # CartPole-v1 has 4 variables in each observation (change for other tasks)
 pset = gp.PrimitiveSet("MAIN", obs_size) 
 
-env_noviz = gym.make("CartPole-v1")
-env_viz = gym.make("CartPole-v1", render_mode="human")
+env_noviz = gym.make("CarRacing-v1")
+env_viz = gym.make("CarRacing-v1", render_mode="human")
 
 def action_wrapper(action): #placeholder for action wrapper
     return 0
